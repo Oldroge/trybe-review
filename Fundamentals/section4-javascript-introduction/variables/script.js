b561ec8b1d9c90dd15b08d223fc4a49a38a8ef07
oldroge <rogeriomrjr@gmail.com>
2022-11-30 18:38:51 -0300

    exercise: trying to re-assign a const variable and throws an error

--- Fundamentals/section4-javascript-introduction/variables/script.js
+++ Fundamentals/section4-javascript-introduction/variables/script.js
@@ -2,6 +2,8 @@ const myName = "Roge";
 const birthCity = "Joinville";
 let birthYear = 1994;
 birthYear = 2030;
+// Esta variável irá lançar um erro pois variáveis constantes não podem ser reatribuídas.
+birthCity = "Florianópolis";
 
 console.log(myName)
 console.log(birthCity)
